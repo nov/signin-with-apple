0c96a99912191ee02f91fc5fe526d21d08b576be
nov <nov@matake.jp>
2019-06-06 17:55:45 +0900

    update apple_id gem to v0.2.0

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,10 @@ class SessionsController < ApplicationController
     when params[:code] && session[:state] == params[:state]
       client.authorization_code = params[:code]
       token_response = client.access_token!
-      token_response.id_token.verify! client, access_token: token_response.access_token
+      token_response.id_token.verify!(
+        client: client,
+        access_token: token_response.access_token
+      )
       session[:id_token] = token_response.id_token.original_jwt.to_s
       redirect_to session_url
     else
