7abda839f48f45daf9d2fe89a84fdd891d98f42d
nov <nov@matake.jp>
2019-06-07 15:09:43 +0900

    play with apple js sdk

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  before_filter :setup_state, only: :new
+  before_filter :setup_client, only: :create
+
   def show
     if session[:id_token]
       @id_token = JSON::JWT.decode session[:id_token], :skip_verification
@@ -12,6 +15,7 @@ class SessionsController < ApplicationController
 
   def create
     case
+<<<<<<< HEAD
     when request.post?
       session[:state] = SecureRandom.hex(8)
       redirect_to client.authorization_uri(
@@ -27,13 +31,20 @@ class SessionsController < ApplicationController
       session[:id_token] = token_response.id_token.original_jwt.to_s
       redirect_to session_url
     else
+=======
+    when params[:error].present?
+>>>>>>> play with apple js sdk
       redirect_to root_url
+    when params[:code].present? && session[:state] == params[:state]
+      receive_authorization_response
+    else
+      send_authorization_request
     end
   end
 
   private
 
-  def client
+  def setup_client
     @client ||= AppleID::Client.new(
       identifier: ENV['CLIENT_ID'],
       team_id: ENV['TEAM_ID'],
@@ -41,5 +52,27 @@ class SessionsController < ApplicationController
       private_key: OpenSSL::PKey::EC.new(ENV['PRIVATE_KEY']),
       redirect_uri: ENV['REDIRECT_URI']
     )
+  end
+
+  def setup_state
+    session[:state] = SecureRandom.hex(8)
+  end
+
+  def send_authorization_request
+    redirect_to @client.authorization_uri(
+      scope: [:email, :name],
+      state: session[:state]
+    )
+  end
+
+  def receive_authorization_response
+    @client.authorization_code = params[:code]
+    token_response = @client.access_token!
+    token_response.id_token.verify!(
+      client: @client,
+      access_token: token_response.access_token
+    )
+    session[:id_token] = token_response.id_token.original_jwt.to_s
+    redirect_to session_url
   end
 end
