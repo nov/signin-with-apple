8cbc6fe8e52d78556bc6cba5989967fa647313f7
nov <matake.nobukazu@moneyforward.co.jp>
2022-07-12 15:34:05 +0900

    up

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    logger.info request.body
+    logger.info request.body.read
     head 200
   end
 end
